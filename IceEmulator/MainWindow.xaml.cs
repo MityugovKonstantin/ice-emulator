8b1e60b55ecbdfd96b29633dafa93b7f89516ec1
Vsevolod Shelihov <vsevolod-777@mail.ru>
2023-03-25 17:55:30 +0700

    Added logger

--- IceEmulator/MainWindow.xaml.cs
+++ IceEmulator/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using GUI;
+using System;
+using System.ComponentModel;
 using System.Windows;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
 
 namespace IceEmulator
 {
@@ -8,6 +12,31 @@ namespace IceEmulator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartStop_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (StartStop_Button.Content.ToString() == "СТАРТ")
+            {
+                Logger_RichTextBox.AppendText($"Двигатель получил команду [{StartStop_Button.Content}]\n");
+                StartStop_Button.Content = "СТОП";
+            }
+            else if (StartStop_Button.Content.ToString() == "СТОП")
+            {
+                Logger_RichTextBox.AppendText($"Двигатель получил команду [{StartStop_Button.Content}]\n");
+                StartStop_Button.Content = "СТАРТ";
+            }
+
+            if (StartStop_Button.Content.ToString() == "СТОП")
+                Logger_RichTextBox.AppendText($"Делаем вид что всё нормально работает\n");
+        }
+
+        private void Logger_RichTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            Logger_RichTextBox.ScrollToEnd();
+            
+           // Logger_RichTextBox.SelectionStart = Logger_RichTextBox.Text.Length;
+           // Logger_RichTextBox.ScrollToCaret();
         }
     }
 }
